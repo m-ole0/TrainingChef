4f897e0ed181d1fd2dafaabfbf7628fd4bb18ed9
m-ole0 <138345360+m-ole0@users.noreply.github.com>
2023-09-10 13:20:29 +0900

    feature_follow

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -5,10 +5,17 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
   end
 
   def update
-
+    @user = current_user
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー情報を更新しました。"
+      redirect_to user_path(@user)
+    else
+      render "edit"
+    end
   end
 
   def favorite_recipes
@@ -18,9 +25,19 @@ class Public::UsersController < ApplicationController
   end
 
   def check
+    @user = current_user
   end
 
   def destroy
+    @user = current_user
+    @user.destroy
+    flash[:notice] = "ユーザーデータを削除しました。"
+    redirect_to root_path
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :email, :profile_image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,15 +11,33 @@ class User < ApplicationRecord
 # フォローした、されたの関係
   has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
 # フォローフォロワー一覧用
   has_many :following_users, through: :followers, source: :followed
   has_many :follower_users, through: :followeds, source: :follower
 
   validates :name, presence: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: "は半角英数字で入力してください" }, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
 
   has_one_attached :profile_image
 
+  #　フォローしたときの処理
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
+
+  #　フォローを外すときの処理
+  def unfollow(user_id)
+    followers.find_by(followed_id: user_id).destroy
+  end
+
+  #フォローしていればtrueを返す
+  def following?(user)
+    following_users.include?(user)
+  end
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : "noimage.png"
+  end
 
 end
