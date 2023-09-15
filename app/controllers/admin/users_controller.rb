bee2008512a949548a82449f6eac592613ad7a42
m-ole0 <138345360+m-ole0@users.noreply.github.com>
2023-09-15 16:10:11 +0900

    管理者側ユーザー編集削除機能実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,21 +1,40 @@
 class Admin::UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+     @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
-
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー情報を更新しました。"
+      redirect_to admin_user_path(@user)
+    else
+      render "edit"
+    end
   end
 
   def check
+    @user = User.find(params[:id])
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "ユーザーデータを削除しました。"
+    redirect_to admin_users_path
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :emaii, :profile_image)
   end
 end
